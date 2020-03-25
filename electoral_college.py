1fae556f20cecac11b05c588984c6d74acd1f9f1
Owen <owen.allemang@gmail.com>
2020-03-25 12:31:08 +0100

    add dataset census 2016

--- electoral_college.py
+++ electoral_college.py
@@ -1,2 +1,7 @@
-from pulp import *
+from imports import *
+
+X = pd.read_csv('dataset/ACSDP5Y2016.DP05_data_with_overlays_2020-03-25T063508.csv',
+                usecols=['NAME', 'DP05_0001E', 'DP05_0082E'])
+
+N = 538     # Number of member in the electoral college
 
