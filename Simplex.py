25777a76c16628d44230affed576761de216bc96
Owen <owen.allemang@gmail.com>
2020-04-21 14:50:03 +0200

    Rework path csv

--- Simplex.py
+++ Simplex.py
@@ -1,2 +1,42 @@
 from imports import *
 
+
+x1 = pulp.LpVariable("x1", 0)
+x2 = pulp.LpVariable("x2", 0)
+x3 = pulp.LpVariable("x3", 0)
+x4 = pulp.LpVariable("x4", 0)
+# x5 = pulp.LpVariable("x5", 0)
+
+
+prob = pulp.LpProblem("Morra", pulp.LpMaximize)
+
+prob += x1 + x2 + x3 + x4
+prob += 8*x1 + 5*x2 + 4*x3 + 7*x4 <= 1
+prob += 2*x1 + 12*x2 + 1*x3 + 8*x4 <= 1
+prob += 6*x1 + 3*x2 + 8*x3 + 5*x4 <= 1
+prob += 5*x1 + 5*x2 + 11*x3 + 12*x4 <= 1
+# prob += 3*x1 + 2*x2 + 1*x3 + 1*x4 + 3*x5 <= 1
+# prob += 1*x1 + 3*x2 + 2*x3 + 3*x4 + 1*x5 <= 1
+# prob += 1*x1 + 3*x2 + 1*x3 + 2*x4 + 3*x5 <= 1
+# prob += 3*x1 + 1*x2 + 3*x3 + 1*x4 + 2*x5 <= 1
+
+status = prob.solve()
+
+print("\n x1 = ", pulp.value(x1),
+      "\n x2 = ", pulp.value(x2),
+      "\n x3 = ", pulp.value(x3),
+      "\n x4 = ", pulp.value(x4),
+      # "\n x5 = ", pulp.value(x5),
+      "\n")
+print(pulp.LpStatus[status], "\n")
+
+z = [pulp.value(x1),
+         pulp.value(x2),
+         pulp.value(x3),
+         pulp.value(x4),
+         ]
+
+zt = sum(z)
+
+print("z = ", zt, "\n v = ", 1/zt)
+print(*[i/zt for i in z])
--- electoral_college.py
+++ electoral_college.py
@@ -1,11 +1,11 @@
 from imports import *
 
-df = pd.read_csv('dataset/ACSDP5Y2016.DP05_data_with_overlays_2020-03-25T063508.csv',
-                usecols=['NAME', 'DP05_0001E', 'DP05_0082E'])
+path = os.path.join("./dataset", "ACSDP5Y2016.DP05_data_with_overlays_2020-03-25T063508.csv")
+
+df = pd.read_csv(path, usecols=['NAME', 'DP05_0001E', 'DP05_0082E'])
 
 # First row to header
 df.columns = df.iloc[0]
 df = df[1:]
 
-N = 538     # Number of member in the electoral college
-
+N = 538  # Number of member in the electoral college
