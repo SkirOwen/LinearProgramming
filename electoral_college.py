993ca30aefe7a196ad3e2d2ccb5ec475d0a6af15
Owen <owen.allemang@gmail.com>
2020-04-21 13:58:37 +0200

    add dataset census 2016

--- electoral_college.py
+++ electoral_college.py
@@ -1,7 +1,11 @@
 from imports import *
 
-X = pd.read_csv('dataset/ACSDP5Y2016.DP05_data_with_overlays_2020-03-25T063508.csv',
+df = pd.read_csv('dataset/ACSDP5Y2016.DP05_data_with_overlays_2020-03-25T063508.csv',
                 usecols=['NAME', 'DP05_0001E', 'DP05_0082E'])
+
+# First row to header
+df.columns = df.iloc[0]
+df = df[1:]
 
 N = 538     # Number of member in the electoral college
 
